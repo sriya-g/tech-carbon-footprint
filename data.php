a3066fc0d8df16b0412c961892918cf3f8c9e2d9
Sriya G <75285747+sriya-g@users.noreply.github.com>
2023-04-01 16:33:50 +0000

    homepage new yay

--- data.php
+++ data.php
@@ -9,8 +9,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-  <?php $diffwebs = $_POST["q1"]; 
-$hrsstreaming = $_POST["q2"];
+  <?php $diffWebs = $_POST["q1"]; 
+$hrsStreaming = $_POST["q2"];
 $hrsZoomTogether = $_POST["q3"];
 $hrsZoom1to1 = $_POST["q32"];
 $hrsmining = $_POST["q4"]; 
@@ -19,8 +19,10 @@ $numPhotos = $_POST["q6"];
 $numSearches = $_POST["q7"];
 $numTweets = $_POST["q8"];
 $numTexts = $_POST["q9"];
-$amtCarbonEmitted = $diffWebs*4.61 + $hrsStreaming*55 + $Zoom1to1*3.7 + $hrsZoomTogether*50 + $numEmails*13 + $numPhotos*50 + $numSearches*0.2 + $numTweets*0.02 + $numTexts*0.014; //in GRAMS
+$amtCarbonEmitted = $diffWebs*4.61 + $hrsStreaming*55 + $hrsZoom1to1*3.7 + $hrsZoomTogether*50 + $numEmails*13 + $numPhotos*50 + $numSearches*0.2 + $numTweets*0.02 + $numTexts*0.014; //in GRAMS
 $amtCarbonEmitted = $amtCarbonEmitted/1000;
 ?>
+<p>Your tech use emits about <?php echo $amtCarbonEmitted ?> kilograms of carbon dioxide per day.</p>
+  <p>That's about <?php echo $amtCarbonEmitted*365 ?> kilograms per year</p>
 </body>
 </html>
