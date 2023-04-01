868aecefc5affe0870f6e3b9d36810c1161a3331
Sriya G <75285747+sriya-g@users.noreply.github.com>
2023-04-01 15:30:26 +0000

    everything

--- data.php
+++ data.php
@@ -1,16 +1,14 @@
 <html>
-  <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>replit</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet"> 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>replit</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
   <?php $elec = $_POST["q1"]; ?>
-  $ spent on electricity: <?php echo $_POST["q1"]; ?><br>
-
 </body>
 </html>
