25f23bfdd0671648997d8f9cfc833f52f3ac810b
Sriya G <75285747+sriya-g@users.noreply.github.com>
2023-04-01 20:51:26 +0000

    stats

---
+++ script2.js
@@ -0,0 +1,4 @@
+var personButton = Document.getElementById("consumer-button");
+var businessButton = Document.getElementById("business-button");
+personButton.addEventListener("click", displayPersFact);
+businessButton.addEventListener("click", displayBusiFact);
