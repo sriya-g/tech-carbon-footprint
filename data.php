bbafb923dc542e4b591bb5a540a38c89dcb74818
Sriya G <75285747+sriya-g@users.noreply.github.com>
2023-04-01 16:13:04 +0000

    calculate carbon footprint

--- data.php
+++ data.php
@@ -9,6 +9,18 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-  <?php $elec = $_POST["q1"]; ?>
+  <?php $diffwebs = $_POST["q1"]; 
+$hrsstreaming = $_POST["q2"];
+$hrsZoomTogether = $_POST["q3"];
+$hrsZoom1to1 = $_POST["q32"];
+$hrsmining = $_POST["q4"]; 
+$numEmails = $_POST["q5"];
+$numPhotos = $_POST["q6"];
+$numSearches = $_POST["q7"];
+$numTweets = $_POST["q8"];
+$numTexts = $_POST["q9"];
+$amtCarbonEmitted = $diffWebs*4.61 + $hrsStreaming*55 + $Zoom1to1*3.7 + $hrsZoomTogether*50 + $numEmails*13 + $numPhotos*50 + $numSearches*0.2 + $numTweets*0.02 + $numTexts*0.014; //in GRAMS
+$amtCarbonEmitted = $amtCarbonEmitted/1000;
+?>
 </body>
 </html>
